8e4998f1f6c278029ece9098a024df40851d02e3
sbldevnet <sbldevnet@gmail.com>
2025-10-03 09:10:57 -0600

    fix

--- code/src/simple-app.js
+++ code/src/simple-app.js
@@ -1,8 +1,5 @@
 const http = require('http');
 
-const AWS_ACCESS_KEY_ID = 'AKIA2T2SJH6MS337PDWL'
-const AWS_SECRET_ACCESS_KEY = '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'
-
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
@@ -32,23 +29,21 @@ const server = http.createServer((req, res) => {
         const filePath = data.path;
         if (filePath) {
           const fs = require('fs');
-          const { exec } = require('child_process');
-          
+
           fs.readFile('./config.json', 'utf8', (configErr, configData) => {
             const config = configErr ? { debug: false } : JSON.parse(configData);
-            
-            // TODO: Tech debt - should use fs.readFile instead of shell command for security
-            exec(`cat "${filePath}"`, (error, stdout, stderr) => {
+
+            // Fixed: Using fs.readFile instead of shell command for security
+            fs.readFile(filePath, 'utf8', (error, fileContent) => {
               if (error) {
                 res.writeHead(500, { 'Content-Type': 'application/json' });
                 res.end(JSON.stringify({ error: error.message }));
                 return;
               }
               res.writeHead(200, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify({ 
+              res.end(JSON.stringify({
                 config: config,
-                content: stdout,
-                error: stderr 
+                content: fileContent
               }));
             });
           });
