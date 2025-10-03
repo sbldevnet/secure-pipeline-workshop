3819462509a3affa405cb11417d23d5695d265f1
sbldevnet <sbldevnet@gmail.com>
2025-10-03 09:49:28 -0600

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -130,7 +130,7 @@ resource "aws_security_group" "ecs_tasks" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.168.0.0/24"]
   }
 
   # trivy:ignore:AVD-AWS-0104
@@ -139,7 +139,7 @@ resource "aws_security_group" "ecs_tasks" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.168.0.0/24"]
   }
 
   tags = {
