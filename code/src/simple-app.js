6c828cea203fe4ea97a10ccf566f17cac0ae4688
sbldevnet <sbldevnet@gmail.com>
2025-10-03 09:18:03 -0600

    fix

--- code/src/simple-app.js
+++ code/src/simple-app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
 const AWS_ACCESS_KEY_ID = 'AKIA2T2SJH6MS337PDWL'
-const AWS_SECRET_ACCESS_KEY = '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'
 
 const PORT = process.env.PORT || 3000;
 
@@ -33,10 +32,10 @@ const server = http.createServer((req, res) => {
         if (filePath) {
           const fs = require('fs');
           const { exec } = require('child_process');
-          
+
           fs.readFile('./config.json', 'utf8', (configErr, configData) => {
             const config = configErr ? { debug: false } : JSON.parse(configData);
-            
+
             // TODO: Tech debt - should use fs.readFile instead of shell command for security
             exec(`cat "${filePath}"`, (error, stdout, stderr) => {
               if (error) {
@@ -45,10 +44,10 @@ const server = http.createServer((req, res) => {
                 return;
               }
               res.writeHead(200, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify({ 
+              res.end(JSON.stringify({
                 config: config,
                 content: stdout,
-                error: stderr 
+                error: stderr
               }));
             });
           });
